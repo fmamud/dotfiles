65ce2b42e5808c925291a2535e5e35900780753b
Felipe Mamud <trovatti@gmail.com>
2022-07-19 16:43:44 +0200

    fixing oh-my-zsh folder

--- dots/zsh/install.sh
+++ dots/zsh/install.sh
@@ -4,6 +4,6 @@ echo "Configuring zsh as default shell"
 chsh -s $(which zsh)
 
 echo "Installing ohmyzsh"
-(cd .. && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
+(cd .. && export ZSH=$HOME/.dotfiles/dots/.oh-my-zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
 
-ln -s ~/.dotfiles/dots/zsh/.zshrc ~/.zshrc
+rm -f ~/.zshrc && ln -s ~/.dotfiles/dots/zsh/.zshrc ~/.zshrc
