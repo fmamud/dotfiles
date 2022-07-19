7241d84ba106074dd8564e664d45a92236ee0f54
Felipe Mamud <trovatti@gmail.com>
2022-07-19 16:46:00 +0200

    append zshrc file right after install

--- dots/zsh/install.sh
+++ dots/zsh/install.sh
@@ -4,6 +4,4 @@ echo "Configuring zsh as default shell"
 chsh -s $(which zsh)
 
 echo "Installing ohmyzsh"
-(cd .. && export ZSH=$HOME/.dotfiles/dots/.oh-my-zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
-
-rm -f ~/.zshrc && ln -s ~/.dotfiles/dots/zsh/.zshrc ~/.zshrc
+(cd .. && export ZSH=$HOME/.dotfiles/dots/.oh-my-zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && rm -f ~/.zshrc && ln -s ~/.dotfiles/dots/zsh/.zshrc ~/.zshrc)
